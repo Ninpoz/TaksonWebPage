95d39bb34dd374a40346d9be629a1be5b8bbf5df
Ninpoz <92263460+Ninpoz@users.noreply.github.com>
2024-09-09 15:35:42 +0200

    Working routing + controllers

---
+++ Controllers/OmOssController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaksonWebPage.Controllers
+{
+    public class OmOssController : Controller
+    {
+        private readonly ILogger<OmOssController> _logger;
+
+        public OmOssController(ILogger<OmOssController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/VåraTjänsterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaksonWebPage.Controllers
+{
+    public class VåraTjänsterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
