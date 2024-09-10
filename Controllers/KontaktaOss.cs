3179ce542cdf642ef1cf4ce7b3a69fc085ffc886
Ninpoz <92263460+Ninpoz@users.noreply.github.com>
2024-09-10 15:19:28 +0200

    UX done

---
+++ Controllers/KontaktaOss.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaksonWebPage.Controllers
+{
+    public class KontaktaOss : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
